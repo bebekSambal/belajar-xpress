8963a3bfd647d66fddf783546ea37b8403145d4c
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-02-24 13:37:51 +0700

    Update index.js
    nambah route ke about dan *

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 3000;
 app.get('/',(req,res)=>{ // routing ke beranda (root)
     res.send('hello world');
 })
+app.get('/about',(req,res)=>{ // routing ke about
+    res.send('ini adalah halaman about rek');
+})
+app.get('*',(req,res)=>{ // routing ke selain route diatas
+    res.send('nyasar lu njenk');
+})
 
 app.listen(port, ()=>{
     console.log(`app lagi nguping di http://localhost:${port}`);
